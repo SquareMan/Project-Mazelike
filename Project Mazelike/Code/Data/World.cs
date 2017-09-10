9fff427157a9996047dd55ee67d0f5f4bff1db33
SquareMan <sebastiancasper3@gmail.com>
2017-09-09 23:43:32 -0400

    World file

---
+++ Project Mazelike/Code/Data/World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMazelike {
+    class World {
+        public World() {
+
+        }
+        Map[,] Overworld = new Map[1, 1];
+
+        Map currentMap;
+        Player player;
+
+    }
+}
