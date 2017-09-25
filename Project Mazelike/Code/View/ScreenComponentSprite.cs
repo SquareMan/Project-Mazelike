304a317c8ca05194e270e7e9635937377e728fc4
Sebastian Casper <sebastiancasper3@aol.com>
2017-09-25 11:18:54 -0400

    New Screen Component

---
+++ Project Mazelike/Code/View/ScreenComponentSprite.cs
@@ -0,0 +1,31 @@
+﻿using ProjectMazelike.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectMazelike.View {
+    class ScreenComponentSprite : ScreenComponent {
+        public ScreenComponentSprite(Sprite sprite, Screen screen, DrawLayer layer, DrawSpace space = DrawSpace.World) : base(screen, layer, space) {
+            this.sprite = sprite;
+        }
+
+        public new Vector2 Position {
+            get {
+                return sprite.Position;
+            }
+            set {
+                sprite.Position = value;
+            }
+        }
+
+        Sprite sprite;
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
